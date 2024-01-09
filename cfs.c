b85a059198f1b138bd884accfc8a806ea40fedc0
Entscheidbarkeit <thomex@qq.com>
2024-01-09 02:18:32 +0100

    1.3

--- cfs.c
+++ cfs.c
@@ -9,7 +9,9 @@ struct node* minimal(struct node* root) {
 	}
 	return iterator;
 }
-
+void decay(struct node* n){
+	n->data->runTime -= n->data->decay;
+}
 struct node* cfs(struct process* processes, int time) {
 	struct process* iterator = processes;
 	struct node* root = NULL;
@@ -24,7 +26,8 @@ struct node* cfs(struct process* processes, int time) {
 	for (int i = 0; i < time; i++) {
 		struct node *min = minimal(root);
 		printf("Time: %d PID: %d\n",min->val,min->data->PID);
-		min->data->runTime += 10 - min->data->decay;
+		min->data->runTime += 10;
+		iterate_post_order(root, decay);
 		iterate_post_order(root, free_node);
 		root = NULL;
 		iterator = processes;
